b8cba93314983e32c0a86e2e2bb64e39fa2b21bf
Nils Meyer <nils.meyer@kit.edu>
2020-05-23 00:21:59 +0200

    Fixed a couple of issues with the XML export.

--- meshio/patran_io.py
+++ meshio/patran_io.py
@@ -74,25 +74,26 @@ def read(
     ele_filename = ele_filename or filename.replace(".pat", ".ele")
     if os.path.isfile(ele_filename):
         with open(ele_filename, "r") as f:
-            mesh = read_ele_buffer(f, mesh, element_gids, autoremove)
+            mesh = read_ele_buffer(f, mesh, element_gids)
 
     # if *.xml file is present: Add cell or node data
     xml_filename = xml_filename or filename.replace(".pat", ".xml")
     if os.path.isfile(xml_filename):
-        mesh = read_xml_buffer(
-            xml_filename, mesh, element_gids, point_gids, autoremove
-        )
+        mesh = read_xml_buffer(xml_filename, mesh, element_gids, point_gids)
 
     # if *.nod file is present: Add point data
     nod_filename = nod_filename or filename.replace(".pat", ".nod")
     if os.path.isfile(nod_filename):
         with open(nod_filename, "r") as f:
-            mesh = read_nod_buffer(f, mesh, point_gids, autoremove)
+            mesh = read_nod_buffer(f, mesh, point_gids)
+
+    if autoremove:
+        mesh.prune()
 
     return mesh
 
 
-def read_ele_buffer(f, mesh, element_gids, autoremove):
+def read_ele_buffer(f, mesh, element_gids):
     """Read element based data file."""
 
     name = f.readline().replace(" ", "_").rstrip("\n")
@@ -106,101 +107,100 @@ def read_ele_buffer(f, mesh, element_gids, autoremove):
     for i in range(N):
         line = f.readline().split()
         ID = int(line[0])
-        values = map(float, line[1:])
+        values = list(map(float, line[1:]))
         data[ID] = numpy.array(values)
 
     for elem_type in mesh.cells.keys():
+        if elem_type not in mesh.cell_data.keys():
+            mesh.cell_data[elem_type] = {}
         for pos, gid in enumerate(element_gids[elem_type]):
             try:
                 values = data[gid]
-                if elem_type in mesh.cell_data.keys():
-                    mesh.cell_data[elem_type][name].append(values)
-                else:
-                    mesh.cell_data[elem_type] = {name: [values]}
             except KeyError:
-                if autoremove:
-                    nodes = mesh.cells[elem_type][pos]
-                    mesh.cells[elem_type][pos] = -numpy.ones_like(nodes)
-                else:
-                    print("No data attached to Element %d. Writing NaN." % gid)
-                    values = numpy.nan * numpy.ones(order)
-                    if elem_type in mesh.cell_data.keys():
-                        mesh.cell_data[elem_type][name].append(values)
-                    else:
-                        mesh.cell_data[elem_type] = {name: [values]}
+                values = numpy.nan * numpy.ones(order)
+            if name in mesh.cell_data[elem_type].keys():
+                mesh.cell_data[elem_type][name].append(values)
+            else:
+                mesh.cell_data[elem_type][name] = [values]
 
-    mesh = _delete_cells(mesh)
     return mesh
 
 
-def read_xml_buffer(xml_filename, mesh, element_gids, point_gids, autoremove):
+def read_xml_buffer(xml_filename, mesh, element_gids, point_gids):
     """Read element based data file."""
     import xml.etree.ElementTree as ET
 
     tree = ET.parse(xml_filename)
     root = tree.getroot()
 
-    data = {}
-
     dataset = root.find("Dataset")
     type = dataset.find("DataType").text
     order = int(dataset.find("NumberOfComponents").text)
-    name = dataset.find("DeptVar").get("Name").replace(" ", "_").rstrip("\n")
+    base_name = (
+        dataset.find("DeptVar").get("Name").replace(" ", "_").rstrip("\n")
+    )
     blocks = dataset.find("Blocks")
 
-    for item in blocks.find("Block").find("Data"):
-        ID = int(item.get("ID"))
-        line = item.find("DeptValues").text
-        values = map(float, line.split())
-        data[ID] = numpy.array(values)
+    for block in blocks.findall("Block"):
+        data = {}
+        indep = block.find("IndpVar")
+        if indep is not None:
+            indep_idx = block.get("Index")
+            indep_name = indep.get("Name")
+            indep_value = indep.get("Value")
+            indep_unit = indep.get("Unit")
+            name = "%s_Block:%s_%s:%s%s" % (
+                base_name,
+                indep_idx,
+                indep_name,
+                indep_value,
+                indep_unit,
+            )
+        else:
+            name = base_name
 
-    if "ELDT" in type:
-        for elem_type in mesh.cells.keys():
-            for pos, gid in enumerate(element_gids[elem_type]):
-                try:
-                    values = data[gid]
+        layers = block.findall("Layer") or [block]
+        for layer in layers:
+            for item in layer.find("Data"):
+                ID = int(item.get("ID"))
+                line = item.find("DeptValues").text
+                values = list(map(float, line.split()))
+                data[ID] = numpy.array(values)
+
+        if "ELDT" in type:
+            for elem_type in mesh.cells.keys():
+                if elem_type not in mesh.cell_data.keys():
+                    mesh.cell_data[elem_type] = {}
+                for pos, gid in enumerate(element_gids[elem_type]):
+                    try:
+                        values = data[gid]
+                    except KeyError:
+                        values = numpy.nan * numpy.ones(order)
+
                     if name in mesh.cell_data[elem_type].keys():
                         mesh.cell_data[elem_type][name].append(values)
                     else:
-                        mesh.cell_data[elem_type] = {name: [values]}
-                except KeyError:
-                    if autoremove:
-                        nodes = mesh.cells[elem_type][pos]
-                        mesh.cells[elem_type][pos] = -numpy.ones_like(nodes)
-                    else:
-                        print(
-                            "No data attached to Element %d. Writing NaN."
-                            % gid
-                        )
-                        values = numpy.nan * numpy.ones(order)
-                        if name in mesh.cell_data[elem_type].keys():
-                            mesh.cell_data[elem_type][name].append(values)
-                        else:
-                            mesh.cell_data[elem_type] = {name: [values]}
+                        mesh.cell_data[elem_type][name] = [values]
 
-    elif "NDDT" in type:
-        for gid in point_gids:
-            try:
-                if name in mesh.point_data.keys():
-                    mesh.point_data[name] = numpy.vstack(
-                        (mesh.point_data[name], data[gid])
-                    )
-                else:
-                    mesh.point_data = {name: data[gid]}
-            except KeyError:
-                print("No data attached to Point %d. Writing NaN." % gid)
-                values = numpy.nan * numpy.ones(order)
+        elif "NDDT" in type:
+            for gid in point_gids:
+                try:
+                    values = data[gid]
+                except KeyError:
+                    values = numpy.nan * numpy.ones(order)
+
                 if name in mesh.point_data.keys():
                     mesh.point_data[name] = numpy.vstack(
                         (mesh.point_data[name], values)
                     )
                 else:
-                    mesh.point_data = {name: values}
-        mesh.point_data[name] = numpy.squeeze(mesh.point_data[name])
+                    mesh.point_data[name] = values
+            mesh.point_data[name] = numpy.squeeze(mesh.point_data[name])
+
     return mesh
 
 
-def read_nod_buffer(f, mesh, point_gids, autoremove):
+def read_nod_buffer(f, mesh, point_gids):
     """Read node based data file."""
     node_id_map = {}
     for line, id in enumerate(point_gids):
@@ -219,7 +219,7 @@ def read_nod_buffer(f, mesh, point_gids, autoremove):
         if line:
             line = line.split()
             ID = int(line[0])
-            values = map(float, line[1:])
+            values = list(map(float, line[1:]))
             line = node_id_map[ID]
             array[line, :] = values
 
@@ -312,17 +312,6 @@ def _scan_cells(point_gids, cells):
         for value in numpy.nditer(arr, op_flags=["readwrite"]):
             value[...] = numpy.flatnonzero(point_gids == value)[0]
     return cells
-
-
-def _delete_cells(mesh):
-    for elem_type in mesh.cells.keys():
-        mask = ~(mesh.cells[elem_type] < 0).any(axis=1)
-        mesh.cells[elem_type] = mesh.cells[elem_type][mask]
-        print(
-            "automatically removed %d cells"
-            % (numpy.size(mask) - numpy.sum(mask))
-        )
-    return mesh
 
 
 def write(filename, mesh):
